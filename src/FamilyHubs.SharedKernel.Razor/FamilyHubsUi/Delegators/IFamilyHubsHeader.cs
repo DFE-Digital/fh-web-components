15be3a80727d92e61b9d3cc98edf901c018507c1
Phil Davies <github@phildavies.co.uk>
2023-05-18 15:09:17 +0100

    allow consumer to not render a menu item

--- src/FamilyHubs.SharedKernel.Razor/FamilyHubsUi/Delegators/IFamilyHubsHeader.cs
+++ src/FamilyHubs.SharedKernel.Razor/FamilyHubsUi/Delegators/IFamilyHubsHeader.cs
@@ -1,10 +1,16 @@
 ﻿using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
 
-namespace FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators
+namespace FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators;
+
+public enum LinkStatus
 {
-    public interface IFamilyHubsHeader
-    {
-        bool ShowNavigationMenu => false;
-        bool IsActive(LinkOptions link);
-    }
+    Visible,
+    NotVisible, // NotRendered? (we might want a Hidden also, for rendered, but html hidden)
+    Active
+}
+
+public interface IFamilyHubsHeader
+{
+    bool ShowNavigationMenu => true;
+    LinkStatus GetStatus(LinkOptions link) => LinkStatus.Visible;
 }
