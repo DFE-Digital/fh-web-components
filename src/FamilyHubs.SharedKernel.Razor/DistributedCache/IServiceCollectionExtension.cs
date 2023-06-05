7b608411d47c2b5a585ec75af2400ccb31ea3c9a
Phil Davies <github@phildavies.co.uk>
2023-06-05 10:16:11 +0100

    add redis support

---
+++ src/FamilyHubs.SharedKernel.Razor/DistributedCache/IServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class IServiceCollectionExtension
+{
+    // passing Action<ReferralDistributedCacheOptions> would follow the standard pattern
+    public static IServiceCollection AddReferralDistributedCache(
+        this IServiceCollection services,
+        string? connectionString,
+        int slidingExpirationInMinutes,
+        string instanceName)
+    {
+        ArgumentNullException.ThrowIfNull(connectionString);
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = connectionString;
+            options.InstanceName = instanceName;
+        });
+
+        // there's currently only one, so this should be fine
+        services.AddSingleton(new DistributedCacheEntryOptions
+        {
+            SlidingExpiration = TimeSpan.FromMinutes(slidingExpirationInMinutes)
+        });
+
+        return services;
+    }
+}
