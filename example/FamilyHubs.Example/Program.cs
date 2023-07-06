60be087d7830cf6de1be2527fea50807cdc6a8cf
Phil Davies <github@phildavies.co.uk>
2023-07-06 15:00:13 +0100

    example site wip

--- example/FamilyHubs.Example/Program.cs
+++ example/FamilyHubs.Example/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddFamilyHubs(builder.Configuration);
 
 var app = builder.Build();
 
