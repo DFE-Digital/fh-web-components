7a1a0177fb91068eb6e4a3ea983e858fba22ba02
Phil Davies <github@phildavies.co.uk>
2023-07-06 16:20:15 +0100

    pagination example wip

--- example/FamilyHubs.Example/Pages/Components/Pagination/Index.cshtml.cs
+++ example/FamilyHubs.Example/Pages/Components/Pagination/Index.cshtml.cs
@@ -1,12 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
+using FamilyHubs.SharedKernel.Razor.Pagination;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FamilyHubs.Example.Pages.Components.Pagination
+namespace FamilyHubs.Example.Pages.Components.Pagination;
+
+public class LargeSetLinkPagination : LargeSetPagination, ILinkPagination
 {
-    public class IndexModel : PageModel
+    public LargeSetLinkPagination(int totalPages, int currentPage)
+        : base(totalPages, currentPage)
     {
-        public void OnGet()
-        {
-        }
+    }
+
+    public string GetUrl(int page)
+    {
+        return "/Components/Pagination?page={page}";
+    }
+}
+
+public class DontShowLinkPagination : DontShowPagination, ILinkPagination
+{
+    public string GetUrl(int page)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class IndexModel : PageModel
+{
+    //todo: static instance in Pagination, so don't keep creating instances
+    public IPagination Pagination { get; set; } = new DontShowPagination();
+    public ILinkPagination LinkPagination { get; set; } = new DontShowLinkPagination();
+
+    public void OnGet(int page)
+    {
+        Pagination = new LargeSetPagination(20, 10);
+        LinkPagination = new LargeSetLinkPagination(20, 10);
     }
 }
