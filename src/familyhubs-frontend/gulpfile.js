9efad8d80ffc38c3fb562cbb1d54a77250ecf7ab
Phil Davies <github@phildavies.co.uk>
2023-07-07 14:28:12 +0100

    wip

--- src/familyhubs-frontend/gulpfile.js
+++ src/familyhubs-frontend/gulpfile.js
@@ -4,18 +4,16 @@ const gulp = require("gulp"),
     rename = require('gulp-rename'),
     fs = require('fs');
 
-//gulp.task('copy-wwwroot', function () {
-//    return gulp.src('wwwroot/**/*')
-//        .pipe(gulp.dest('../../wwwroot'));
-//});
-
+//todo: not working for the local reference (for now need to manually run gulp copy-familyhubs-frontend-js from node_modules/familyhubs-frontend)
 // return true if being installed from NPM, false if being run from the local repo (using file:)
 function remotelyInstalled() {
-    return process.cwd().endsWith('node_modules/familyhubs-frontend');
+    return process.cwd().endsWith('node_modules\familyhubs-frontend');
 }
 
 function getWwwRootDir() {
-    let baseDir = remotelyInstalled() ? '../..' : '.';
+    //console.log(process.cwd());
+    //console.log(remotelyInstalled());
+    let baseDir = remotelyInstalled() ? '../..' : '../..';
 
     // Check if this is a NPM link situation
     //if (process.env.npm_lifecycle_event === 'link') {}
@@ -39,6 +37,8 @@ function copyPackageJsToWwwroot(packageName, srcFilename) {
 
     let baseDir = getWwwRootDir();
 
+    //console.log(process.cwd());
+    //console.log(baseDir);
     // Copy and rename the file
     return gulp.src(`../${packageName}/${srcFilename}`)
         .pipe(rename(destFileName))
