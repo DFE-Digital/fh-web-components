d394c3e588be0c457e5e5bca9fdac1c18e0ff002
Phil Davies <github@phildavies.co.uk>
2023-07-13 12:23:18 +0100

    wip

---
+++ src/FamilyHubs.SharedKernel.Razor/Urls/HtmlHelperFamilyHubExtensions.cs
@@ -0,0 +1,21 @@
+﻿using FamilyHubs.SharedKernel.Razor.FamilyHubsUi;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FamilyHubs.SharedKernel.Razor.Urls;
+
+public static class HtmlHelperFamilyHubExtensions
+{
+    public static string FamilyHubUrl<TUrlKeyEnum>(this IHtmlHelper htmlHelper, TUrlKeyEnum baseUrl, string? url)
+        where TUrlKeyEnum : struct, Enum
+    {
+        var model = htmlHelper.ViewData["FamilyHubsLayoutModel"] as FamilyHubsLayoutModel;
+
+        var baseUrlString = baseUrl.ToString();
+        if (model!.FamilyHubsUiOptions.Value.Urls.TryGetValue(baseUrlString, out var baseUrlValue))
+        {
+            return $"{baseUrlValue}{url}";
+        }
+
+        return url ?? "";
+    }
+}
