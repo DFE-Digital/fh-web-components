dfa83f300719cb49d5e9f3f09f181b0a1f018e35
Phil Davies <github@phildavies.co.uk>
2023-07-13 13:01:52 +0100

    wip

--- src/FamilyHubs.SharedKernel.Razor/Urls/HtmlHelperFamilyHubExtensions.cs
+++ src/FamilyHubs.SharedKernel.Razor/Urls/HtmlHelperFamilyHubExtensions.cs
@@ -1,21 +1,32 @@
 ﻿using FamilyHubs.SharedKernel.Razor.FamilyHubsUi;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FamilyHubs.SharedKernel.Razor.Urls;
 
 public static class HtmlHelperFamilyHubExtensions
 {
-    public static string FamilyHubUrl<TUrlKeyEnum>(this IHtmlHelper htmlHelper, TUrlKeyEnum baseUrl, string? url)
+    public static Uri FamilyHubUrl<TUrlKeyEnum>(this IHtmlHelper htmlHelper, TUrlKeyEnum baseUrl, string? url)
         where TUrlKeyEnum : struct, Enum
     {
         var model = htmlHelper.ViewData["FamilyHubsLayoutModel"] as FamilyHubsLayoutModel;
 
+        //todo: possibly cache from config as Uri's?
         var baseUrlString = baseUrl.ToString();
-        if (model!.FamilyHubsUiOptions.Value.Urls.TryGetValue(baseUrlString, out var baseUrlValue))
+        if (!model!.FamilyHubsUiOptions.Value.Urls.TryGetValue(baseUrlString, out var baseUrlValue))
         {
-            return $"{baseUrlValue}{url}";
+            throw new ArgumentException($"No url found in FamilyHubsUi:Urls for key \"{baseUrlString}\"", nameof(baseUrl));
         }
 
-        return url ?? "";
+        return new Uri(new Uri(baseUrlValue), url);
+    }
+
+    public static Uri FamilyHubConfigUrl<TUrlKeyEnum>(this IHtmlHelper htmlHelper, TUrlKeyEnum baseUrl, string configKey)
+        where TUrlKeyEnum : struct, Enum
+    {
+        var configuration = htmlHelper.ViewContext.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+        return htmlHelper.FamilyHubUrl(baseUrl, configuration[configKey]);
     }
 }
