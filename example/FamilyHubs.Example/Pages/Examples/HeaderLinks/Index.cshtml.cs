79b8ed6a224923ea9ccd1a8326249736517c03ae
Phil Davies <github@phildavies.co.uk>
2023-08-24 14:57:40 +0100

    add new example page and update version

---
+++ example/FamilyHubs.Example/Pages/Examples/HeaderLinks/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Delegators;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.Example.Pages.Examples.HeaderLinks;
+
+public class IndexModel : PageModel, IFamilyHubsHeader
+{
+}
