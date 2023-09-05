949ec3a394189a15448a510b72d962b331a67fbe
Phil Davies <github@phildavies.co.uk>
2023-09-05 11:40:06 +0100

    add support for a path prefix for included files

--- src/FamilyHubs.SharedKernel.Razor/FamilyHubsUi/Options/IFamilyHubsUiOptions.cs
+++ src/FamilyHubs.SharedKernel.Razor/FamilyHubsUi/Options/IFamilyHubsUiOptions.cs
@@ -3,7 +3,11 @@
 //todo: any value in this interface? can't easily inject it using IOptions<IFamilyHubsUiOptions>
 public interface IFamilyHubsUiOptions
 {
+    /// <summary>
+    /// The name of the service, as displayed in the header and elsewhere.
+    /// </summary>
     string ServiceName { get; set; }
+
     Phase Phase { get; set; }
     string FeedbackUrl { get; set; }
 
@@ -11,6 +15,13 @@ public interface IFamilyHubsUiOptions
     /// The support email address for the service, as displayed on error pages.
     /// </summary>
     string SupportEmail { get; set; }
+
+    /// <summary>
+    /// If set, the path prefix will be prepended to all files included through the layout,
+    /// e.g. css, js and asset files.
+    /// Useful for when the site is being used behind an App Gateway using path based routing.
+    /// </summary>
+    public string PathPrefix { get; set; }
 
     Dictionary<string, string> Urls { get; set; }
     AnalyticsOptions? Analytics { get; set; }
