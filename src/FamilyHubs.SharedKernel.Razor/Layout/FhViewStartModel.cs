bf4f4feedc2c28676cd67ec16a14d3e3793cc5a1
Phil Davies <github@phildavies.co.uk>
2023-09-07 15:46:03 +0100

    now works - needs tidying

--- src/FamilyHubs.SharedKernel.Razor/Layout/FhViewStartModel.cs
+++ src/FamilyHubs.SharedKernel.Razor/Layout/FhViewStartModel.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using FamilyHubs.SharedKernel.Razor.AlternativeServices;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi;
+using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Extensions;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Options;
 
 namespace FamilyHubs.SharedKernel.Razor.Layout;
 
 public class FhViewStartModel
 {
-    public FhViewStartModel(PageModel pageModel, ViewDataDictionary<PageModel> viewData)
+    //public FhViewStartModel(PageModel pageModel, ViewDataDictionary<PageModel> viewData)
+    //{
+    //    PageModel = pageModel;
+    //    ViewData = viewData;
+    //}
+
+    public static void ViewStartInit(FamilyHubsLayoutModel familyHubsLayoutModel, PageModel pageModel, ViewDataDictionary<PageModel> viewData)
     {
-        PageModel = pageModel;
-        ViewData = viewData;
+        familyHubsLayoutModel.PageModel = pageModel;
+        var alt = familyHubsLayoutModel.PageModel as IAlternativeService;
+        if (alt?.ServiceName != null)
+        {
+            var altFamilyHubsUiOptions = familyHubsLayoutModel.FamilyHubsUiOptions.Value.AlternativeFamilyHubsUi[alt.ServiceName];
+            if (altFamilyHubsUiOptions.Enabled)
+            {
+                familyHubsLayoutModel.FamilyHubsUiOptions = Options.Create(altFamilyHubsUiOptions);
+            }
+        }
+        viewData.SetFamilyHubsLayoutModel(familyHubsLayoutModel);
     }
 
-    public Microsoft.AspNetCore.Mvc.RazorPages.PageModel PageModel;
-    public ViewDataDictionary<Microsoft.AspNetCore.Mvc.RazorPages.PageModel> ViewData;
+    //public PageModel PageModel;
+    //public ViewDataDictionary<PageModel> ViewData;
 }
