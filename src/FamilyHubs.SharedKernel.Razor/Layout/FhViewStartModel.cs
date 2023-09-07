23610ded235c838064826a2d591fe7bbe6ab03e4
Phil Davies <github@phildavies.co.uk>
2023-09-07 15:39:33 +0100

    wip (not working)

---
+++ src/FamilyHubs.SharedKernel.Razor/Layout/FhViewStartModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace FamilyHubs.SharedKernel.Razor.Layout;
+
+public class FhViewStartModel
+{
+    public FhViewStartModel(PageModel pageModel, ViewDataDictionary<PageModel> viewData)
+    {
+        PageModel = pageModel;
+        ViewData = viewData;
+    }
+
+    public Microsoft.AspNetCore.Mvc.RazorPages.PageModel PageModel;
+    public ViewDataDictionary<Microsoft.AspNetCore.Mvc.RazorPages.PageModel> ViewData;
+}
