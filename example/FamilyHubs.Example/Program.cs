642c31c20ed6ff22085ea73a2c0d28a14c1e13f0
Phil Davies <github@phildavies.co.uk>
2023-09-12 14:54:03 +0100

    add security headers (and error handling) to example site

--- example/FamilyHubs.Example/Program.cs
+++ example/FamilyHubs.Example/Program.cs
@@ -9,10 +9,11 @@ builder.Services.AddFamilyHubs(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseFamilyHubs();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
