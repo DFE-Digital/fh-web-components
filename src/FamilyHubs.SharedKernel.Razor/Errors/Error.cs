8a947bc802acf69c932b159b7a35609b453d01d5
Phil Davies <github@phildavies.co.uk>
2023-09-19 13:26:46 +0100

    error handling wip

--- src/FamilyHubs.SharedKernel.Razor/Errors/Error.cs
+++ src/FamilyHubs.SharedKernel.Razor/Errors/Error.cs
@@ -1,3 +1,16 @@
 ﻿namespace FamilyHubs.SharedKernel.Razor.Errors;
 
-public record Error(int Id, string HtmlElementId, string ErrorMessage);
+/// <summary>
+/// Represents an error that is displayed in the error summary and next to the input control.
+/// </summary>
+/// <param name="Id">The error id (usually an enum of error types.)</param>
+/// <param name="HtmlElementId">The id of the input element that gets focus, when the error is clicked/tapped in the error summary.</param>
+/// <param name="ErrorMessage">The error message that is displayed in the error summary and next to the input control.</param>
+public record Error(int Id, string HtmlElementId, string ErrorMessage)
+{
+    /// <summary>
+    /// The id of the error message element that is displayed next to the input control.
+    /// Will be used as the aria-describedby attribute value, when the input is in an error state.
+    /// </summary>
+    public string InputErrorMessageParaId => $"{HtmlElementId}-error-message";
+}
