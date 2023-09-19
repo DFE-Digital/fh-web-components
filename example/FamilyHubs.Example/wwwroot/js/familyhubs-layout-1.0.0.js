7ae3c38b845bfaf61abb7a4b98e4a603d164aaa7
Phil Davies <github@phildavies.co.uk>
2023-09-19 11:55:15 +0100

    remove fh layout js + unneeded error page from example site

--- example/FamilyHubs.Example/wwwroot/js/familyhubs-layout-1.0.0.js
+++
@@ -1,6 +0,0 @@
-/*todo: use typescript and add gulp task to bundle and minify this file,
- but just have it as a js for now as a poc */
-
-document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');
-
-
