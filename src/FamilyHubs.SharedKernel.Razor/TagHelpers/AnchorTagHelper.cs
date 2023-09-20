8d5818978f05a8959accae30b952bdd1346fb157
Phil Davies <github@phildavies.co.uk>
2023-09-20 14:25:23 +0100

    add fh anchor tag helper

---
+++ src/FamilyHubs.SharedKernel.Razor/TagHelpers/AnchorTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FamilyHubs.SharedKernel.Razor.TagHelpers;
+
+[HtmlTargetElement("a", Attributes = "fh-new-tab")]
+public class AnchorTagHelper : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.SetAttribute("target", "_blank");
+        output.Attributes.SetAttribute("rel", "noopener noreferrer");
+        output.PostContent.SetHtmlContent(" (opens in new tab)");
+    }
+}
