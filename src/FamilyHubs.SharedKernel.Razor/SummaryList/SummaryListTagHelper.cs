7af8af8482e5fb2786fc4de5bb12ccdb42af1af9
Phil Davies <github@phildavies.co.uk>
2023-09-27 11:29:29 +0100

    wip

--- src/FamilyHubs.SharedKernel.Razor/SummaryList/SummaryListTagHelper.cs
+++ src/FamilyHubs.SharedKernel.Razor/SummaryList/SummaryListTagHelper.cs
@@ -4,10 +4,12 @@ namespace FamilyHubs.SharedKernel.Razor.SummaryList;
 
 public class SummaryListTagHelper : TagHelper
 {
+    public bool Border { get; set; } = true;
+
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
         output.TagName = "dl";
-        output.Attributes.SetAttribute("class", "govuk-summary-list");
+        output.Attributes.SetAttribute("class", $"govuk-summary-list {(Border?"":"govuk-summary-list--no-border")}");
         output.Content.SetHtmlContent((await output.GetChildContentAsync()).GetContent());
     }
 }
