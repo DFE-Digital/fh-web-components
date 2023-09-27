5c7ff0c7c969262fdd9ff28c2ccaa0fade9fa6c5
Phil Davies <github@phildavies.co.uk>
2023-09-27 11:20:27 +0100

    summary list wip

---
+++ src/FamilyHubs.SharedKernel.Razor/SummaryList/SummaryListTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FamilyHubs.SharedKernel.Razor.SummaryList;
+
+public class SummaryListTagHelper : TagHelper
+{
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "dl";
+        output.Attributes.SetAttribute("class", "govuk-summary-list");
+        output.Content.SetHtmlContent((await output.GetChildContentAsync()).GetContent());
+    }
+}
