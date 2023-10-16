b8f7f4a4277238610daabf8eab8a998693d498c9
Phil Davies <github@phildavies.co.uk>
2023-10-16 14:24:58 +0100

    health check wip

---
+++ src/FamilyHubs.SharedKernel.Razor/Health/HealthChecksBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FamilyHubs.SharedKernel.Razor.Health;
+
+public static class HealthChecksBuilderExtensions
+{
+    public enum UrlType
+    {
+        InternalApi,
+        ExternalApi,
+        InternalSite,
+        ExternalSite
+    }
+
+    public static IHealthChecksBuilder AddApi(
+        this IHealthChecksBuilder builder,
+        string name,
+        string configKey,
+        IConfiguration configuration,
+        UrlType urlType = UrlType.InternalApi)
+    {
+        string? apiUrl = configuration.GetValue<string>(configKey);
+
+        // Only add the health check if the config key is set.
+        // Either the API is optional (or not used locally) and missing intentionally,
+        // in which case there's no need to add the health check,
+        // or it's required, but in that case, the real consumer of the API should
+        // continue to throw it's own relevant exception
+        if (!string.IsNullOrEmpty(apiUrl))
+        {
+            if (urlType == UrlType.InternalApi)
+            {
+                //todo: add "/health" endpoints to all APIs
+
+                apiUrl = apiUrl.TrimEnd('/') + "/api/info";
+            }
+
+            // we handle API failures as Degraded, so that App Services doesn't remove or replace the instance (all instances!) due to an API being down
+            builder.AddUrlGroup(new Uri(apiUrl), name, HealthStatus.Degraded,
+                new[] { urlType.ToString() });
+        }
+
+        return builder;
+    }
+}
