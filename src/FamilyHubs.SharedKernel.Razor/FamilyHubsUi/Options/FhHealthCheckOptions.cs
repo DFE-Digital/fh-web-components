7f08b96aa314668271ce0489786f1805b083ae86
Phil Davies <github@phildavies.co.uk>
2023-10-17 13:09:13 +0100

    tidy up

--- src/FamilyHubs.SharedKernel.Razor/FamilyHubsUi/Options/FhHealthCheckOptions.cs
+++ src/FamilyHubs.SharedKernel.Razor/FamilyHubsUi/Options/FhHealthCheckOptions.cs
@@ -1,69 +1,61 @@
 ﻿
-namespace FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options
+namespace FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
+
+public class HealthCheckDatabaseOptions
 {
-    //public interface IUrlOptions
-    //{
-    //    string? BaseUrlKey { get; set; }
-    //    string? Url { get; set; }
-    //    string? ConfigUrl { get; set; }
-    //}
-
-    public class HealthCheckDatabaseOptions //: IUrlOptions
+    public enum DatabaseType
     {
-        public enum DatabaseType
-        {
-            SqlServer
-        }
-
-        /// <summary>
-        /// The ConnectionString.
-        /// </summary>
-        public string? ConnectionString { get; set; }
-
-        /// <summary>
-        /// If supplied, the ConnectionString is populated from the config value found at the given config key.
-        /// </summary>
-        /// <example>
-        /// "ConnectionStrings:SharedKernelConnection"
-        /// </example>
-        public string? ConfigConnectionString { get; set; }
-
-        public DatabaseType Type { get; set; } = DatabaseType.SqlServer;
+        SqlServer
     }
 
-    public class HealthCheckUrlOptions //: IUrlOptions
-    {
-        /// <summary>
-        /// A key name from the FamilyHubsUi:Url section.
-        /// If supplied, the configured value of the given key name,
-        /// acts as the base URL for the relative Url supplied through Url or ConfigUrl.
-        /// </summary>
-        public string? BaseUrlKey { get; set; }
+    /// <summary>
+    /// The ConnectionString.
+    /// </summary>
+    public string? ConnectionString { get; set; }
 
-        /// <summary>
-        /// The URL for the link.
-        /// </summary>
-        public string? Url { get; set; }
+    /// <summary>
+    /// If supplied, the ConnectionString is populated from the config value found at the given config key.
+    /// </summary>
+    /// <example>
+    /// "ConnectionStrings:SharedKernelConnection"
+    /// </example>
+    public string? ConfigConnectionString { get; set; }
 
-        /// <summary>
-        /// If supplied, the Url is populated from the config value found at the given config key.
-        /// </summary>
-        /// <example>
-        /// "FamilyHubsUi:FeedbackUrl"
-        /// </example>
-        public string? ConfigUrl { get; set; }
-    }
+    public DatabaseType Type { get; set; } = DatabaseType.SqlServer;
+}
 
-    public class FhHealthCheckOptions
-    {
-        public bool Enabled { get; set; }
+public class HealthCheckUrlOptions
+{
+    /// <summary>
+    /// A key name from the FamilyHubsUi:Url section.
+    /// If supplied, the configured value of the given key name,
+    /// acts as the base URL for the relative Url supplied through Url or ConfigUrl.
+    /// </summary>
+    public string? BaseUrlKey { get; set; }
 
-        public Dictionary<string, HealthCheckUrlOptions> InternalApis { get; set; } = new();
+    /// <summary>
+    /// The URL for the link.
+    /// </summary>
+    public string? Url { get; set; }
 
-        public Dictionary<string, HealthCheckUrlOptions> ExternalApis { get; set; } = new();
+    /// <summary>
+    /// If supplied, the Url is populated from the config value found at the given config key.
+    /// </summary>
+    /// <example>
+    /// "FamilyHubsUi:FeedbackUrl"
+    /// </example>
+    public string? ConfigUrl { get; set; }
+}
 
-        public Dictionary<string, HealthCheckUrlOptions> ExternalSites { get; set; } = new();
+public class FhHealthCheckOptions
+{
+    public bool Enabled { get; set; }
 
-        public Dictionary<string, HealthCheckDatabaseOptions> Databases { get; set; } = new();
-    }
+    public Dictionary<string, HealthCheckUrlOptions> InternalApis { get; set; } = new();
+
+    public Dictionary<string, HealthCheckUrlOptions> ExternalApis { get; set; } = new();
+
+    public Dictionary<string, HealthCheckUrlOptions> ExternalSites { get; set; } = new();
+
+    public Dictionary<string, HealthCheckDatabaseOptions> Databases { get; set; } = new();
 }
