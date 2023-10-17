83b7bbebbb5df10dde7930b5bc771435f96f7680
Phil Davies <github@phildavies.co.uk>
2023-10-17 13:07:45 +0100

    tidy up

--- example/FamilyHubs.Example/Program.cs
+++ example/FamilyHubs.Example/Program.cs
@@ -9,19 +9,6 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddFamilyHubs(builder.Configuration);
 
-//todo: gotta find a better way to do this
-// there's a version that has a uri factory, nut we're still buggered for name...
-// public static IHealthChecksBuilder AddUrlGroup(
-// this IHealthChecksBuilder builder,
-// Func<IServiceProvider, Uri> uriProvider,
-
-
-//#pragma warning disable ASP0000
-//var serviceProvider = builder.Services.BuildServiceProvider();
-//#pragma warning restore ASP0000
-//var uiOptions = serviceProvider.GetService<IOptions<FamilyHubsUiOptions>>();
-//var healthCheckBuilder = builder.Services.AddHealthChecks().AddFamilyHubs(uiOptions!.Value);
-
 builder.Services
     .AddHealthChecks()
     .AddFamilyHubs(builder.Configuration);
