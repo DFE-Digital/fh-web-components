666ba8beef72d9a27d2af987ec8d4b73305b66c8
Phil Davies <github@phildavies.co.uk>
2023-10-19 13:57:36 +0100

    summary list wip

--- src/FamilyHubs.SharedKernel.Razor/SummaryList/SummaryListRowTagHelper.cs
+++ src/FamilyHubs.SharedKernel.Razor/SummaryList/SummaryListRowTagHelper.cs
@@ -1,30 +1,77 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace FamilyHubs.SharedKernel.Razor.SummaryList;
 
 public class SummaryListRowTagHelper : TagHelper
 {
+    //todo: Key is mandatory
+    [Required]
     public string? Key { get; set; }
+    public string? Action1 { get; set; }
+    public string? Action1Href { get; set; }
+    public string? Action2 { get; set; }
+    public string? Action2Href { get; set; }
 
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
+        if (string.IsNullOrEmpty(Key))
+        {
+            throw new ArgumentNullException(nameof(Key));
+        }
+
         string finalValue = (await output.GetChildContentAsync()).GetContent();
         if (!string.IsNullOrWhiteSpace(finalValue))
         {
             output.TagName = "div";
             output.Attributes.SetAttribute("class", "govuk-summary-list__row");
-            output.Content.SetHtmlContent($@"
-                <dt class='govuk-summary-list__key'>
-                    {Key}
-                </dt>
-                <dd class='govuk-summary-list__value'>
-                    {finalValue}
-                </dd>
-            ");
+
+            output.Content.SetHtmlContent(
+                $@"<dt class='govuk-summary-list__key'>{Key}</dt>
+                <dd class='govuk-summary-list__value'>{finalValue}</dd>");
+
+            if (!string.IsNullOrWhiteSpace(Action1))
+            {
+                string action = ActionLink(Action1, Action1Href, Key);
+                if (!string.IsNullOrWhiteSpace(Action2))
+                {
+                    action = $"<ul class='govuk-summary-list__actions-list'>{ActionListItem(action)}{ActionListItem(ActionLink(Action2, Action2Href, Key!))}</ul>";
+                }
+                output.Content.AppendHtml($"<dd class=\"govuk-summary-list__actions\">{action}</dd>");
+            }
         }
         else
         {
             output.SuppressOutput();
         }
     }
+
+    private string ActionLink(string action, string? href, string key)
+    {
+        return $"<a href='{href}'>{action}<span class='govuk-visually-hidden'> {key.ToLower(CultureInfo.CurrentCulture)}</span></a>";
+    }
+
+    private string ActionListItem(string actionLink)
+    {
+        return $"<li class='govuk-summary-list__actions-list-item'>{actionLink}</li>";
+    }
 }
+
+//public class SummaryListRow2TagHelper : TagHelper
+//{
+//    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+//    {
+//        string finalValue = (await output.GetChildContentAsync()).GetContent();
+//        if (!string.IsNullOrWhiteSpace(finalValue))
+//        {
+//            output.TagName = "div";
+//            output.Attributes.SetAttribute("class", "govuk-summary-list__row");
+//            output.Content.SetHtmlContent(finalValue);
+//        }
+//        else
+//        {
+//            output.SuppressOutput();
+//        }
+//    }
+//}
