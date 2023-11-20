3559ce5e343c072f99984e1241d0cada12e6ffc4
Phil Davies <github@phildavies.co.uk>
2023-11-20 16:00:13 +0000

    wip

--- src/FamilyHubs.SharedKernel.Razor/FullPages/ISingleTextboxPageModel.cs
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/ISingleTextboxPageModel.cs
@@ -1,11 +1,17 @@
-﻿namespace FamilyHubs.SharedKernel.Razor.FullPages;
+﻿using FamilyHubs.SharedKernel.Razor.Errors;
+
+namespace FamilyHubs.SharedKernel.Razor.FullPages;
+
+//todo: composable concrete?
 
 public interface ISingleTextboxPageModel
 {
     string HeadingText { get; set; }
     string? HintText { get; set; }
     string TextBoxLabel { get; set; }
-    string ErrorText { get; set; }
+    //string ErrorText { get; set; }
     string? TextBoxValue { get; set; }
-    bool HasErrors { get; }
+    //bool HasErrors { get; }
+
+    ErrorState ErrorState { get; set; }
 }
