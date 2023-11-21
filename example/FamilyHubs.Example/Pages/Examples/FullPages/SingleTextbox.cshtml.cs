fcd17287772e498032fc13030ab3f9671984cbc9
Phil Davies <github@phildavies.co.uk>
2023-11-21 09:22:44 +0000

    single textbox page wip

---
+++ example/FamilyHubs.Example/Pages/Examples/FullPages/SingleTextbox.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.Example.Pages.Examples.FullPages
+{
+    public class SingleTextboxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
