b8ed92a15a90897c6b224420aa2f2ae661d3de51
Phil Davies <github@phildavies.co.uk>
2023-11-21 10:43:16 +0000

    single textbox wip

--- example/FamilyHubs.Example/Pages/Examples/FullPages/SingleTextbox.cshtml.cs
+++ example/FamilyHubs.Example/Pages/Examples/FullPages/SingleTextbox.cshtml.cs
@@ -1,20 +1,48 @@
+using System.Collections.Immutable;
 using FamilyHubs.SharedKernel.Razor.Errors;
 using FamilyHubs.SharedKernel.Razor.FullPages;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
-namespace FamilyHubs.Example.Pages.Examples.FullPages
+namespace FamilyHubs.Example.Pages.Examples.FullPages;
+
+/// <summary>
+/// Simple example, not production ready, no P/R/G, loading existing value, retaining value etc.
+/// </summary>
+public class SingleTextboxModel : PageModel, ISingleTextboxPageModel
 {
-    public class SingleTextboxModel : PageModel, ISingleTextboxPageModel
-    {
-        public string HeadingText { get; set; } = "Single Textbox Example";
-        public string? HintText { get; set; } = "It's the ultimate question";
-        public string TextBoxLabel { get; set; } = "What is the answer to life, the universe, and everything?";
-        public string? TextBoxValue { get; set; }
-        public IErrorState Errors { get; set; } = ErrorState.Empty;
+    public string HeadingText { get; set; } = "Single Textbox Example";
+    public string? HintText { get; set; } = "It's the ultimate question";
+    public string TextBoxLabel { get; set; } = "What is the answer to life, the universe, and everything?";
+    public IErrorState Errors { get; set; } = ErrorState.Empty;
 
-        public void OnGet()
+    [Required]
+    [BindProperty]
+    public string? TextBoxValue { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+        if (string.IsNullOrWhiteSpace(TextBoxValue))
         {
+            Errors = ErrorState.Create(PossibleErrors, new[] { ErrorId.AnswerMissing });
         }
     }
+
+    public enum ErrorId
+    {
+        AnswerMissing,
+        AnswerTooLong
+    }
+
+    //todo: helper, so that consumers don't need to fish out the textbox id
+    //todo: combine with passing the field in the view, so view details don't leak into the model
+    public static readonly ImmutableDictionary<int, Error> PossibleErrors =
+        ImmutableDictionary.Create<int, Error>()
+            .Add(ErrorId.AnswerMissing, "textbox", "Guru meditation required")
+            .Add(ErrorId.AnswerTooLong, "textbox", "The answer is too long");
 }
