805fd9e3d276e533545ee9874bccca6519056a96
Phil Davies <github@phildavies.co.uk>
2023-11-23 15:30:08 +0000

    errors next example

---
+++ src/FamilyHubs.SharedKernel.Razor/ErrorNext/ErrorDictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+
+namespace FamilyHubs.SharedKernel.Razor.ErrorNext;
+
+public static class ErrorDictionaryExtensions
+{
+    public static ImmutableDictionary<int, Error> Add<T>(
+        this ImmutableDictionary<int, Error> dictionary,
+        T errorId,
+        string errorMessage)
+        where T : struct, Enum, IConvertible
+    {
+        return dictionary.Add((int)(IConvertible)errorId, new Error((int)(IConvertible)errorId, errorMessage));
+    }
+}
