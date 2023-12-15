391277254e2bb4980d70251d043f1c752a114fe9
Phil Davies <github@phildavies.co.uk>
2023-12-15 11:24:32 +0000

    add fh version of add-another component wip

---
+++ src/familyhubs-frontend/scripts/components/add-another.ts
@@ -0,0 +1,99 @@
+// A version of the MOJ's add-another component what plays nice with the accessible autocomplete component.
+// I did consider subclassing the MOJ's add-another component,
+// but it would have been so coupled that it would've probably broken on an update of the MOJ library.
+// So instead we forked it and made our own version.
+
+window.FamilyHubsFrontend = window.FamilyHubsFrontend || {};
+
+export function initializeAddAnother(): void {
+    //todo: support options with scope?
+    var $addAnothers = document.querySelectorAll('[data-module="fh-add-another"]');
+
+    $addAnothers.forEach(function ($addAnother) {
+		new window.FamilyHubsFrontend.AddAnother($addAnother);
+    });
+}
+
+window.FamilyHubsFrontend.AddAnother = function (container) {
+	this.container = $(container);
+
+	if (this.container.data('fh-add-another-initialised')) {
+		return
+	}
+
+	this.container.data('fh-add-another-initialised', true);
+
+	this.container.on('click', '.fh-add-another__remove-button', $.proxy(this, 'onRemoveButtonClick'));
+	this.container.on('click', '.fh-add-another__add-button', $.proxy(this, 'onAddButtonClick'));
+	this.container.find('.fh-add-another__add-button, fh-add-another__remove-button').prop('type', 'button');
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.onAddButtonClick = function (e) {
+	var item = this.getNewItem();
+	this.updateAttributes(this.getItems().length, item);
+	this.resetItem(item);
+	var firstItem = this.getItems().first();
+	if (!this.hasRemoveButton(firstItem)) {
+		this.createRemoveButton(firstItem);
+	}
+	this.getItems().last().after(item);
+	item.find('input, textarea, select').first().focus();
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.hasRemoveButton = function (item) {
+	return item.find('.fh-add-another__remove-button').length;
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.getItems = function () {
+	return this.container.find('.fh-add-another__item');
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.getNewItem = function () {
+	var item = this.getItems().first().clone();
+	if (!this.hasRemoveButton(item)) {
+		this.createRemoveButton(item);
+	}
+	return item;
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.updateAttributes = function (index, item) {
+	item.find('[data-name]').each(function (i, el) {
+		var originalId = el.id
+
+		el.name = $(el).attr('data-name').replace(/%index%/, index);
+		el.id = $(el).attr('data-id').replace(/%index%/, index);
+
+		var label = $(el).siblings('label')[0] || $(el).parents('label')[0] || item.find('[for="' + originalId + '"]')[0];
+		label.htmlFor = el.id;
+	});
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.createRemoveButton = function (item) {
+	item.append('<button type="button" class="govuk-button govuk-button--secondary fh-add-another__remove-button">Remove</button>');
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.resetItem = function (item) {
+	item.find('[data-name], [data-id]').each(function (index, el) {
+		if (el.type == 'checkbox' || el.type == 'radio') {
+			el.checked = false;
+		} else {
+			el.value = '';
+		}
+	});
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.onRemoveButtonClick = function (e) {
+	$(e.currentTarget).parents('.fh-add-another__item').remove();
+	var items = this.getItems();
+	if (items.length === 1) {
+		items.find('.fh-add-another__remove-button').remove();
+	}
+	items.each($.proxy(function (index, el) {
+		this.updateAttributes(index, $(el));
+	}, this));
+	this.focusHeading();
+};
+
+window.FamilyHubsFrontend.AddAnother.prototype.focusHeading = function () {
+	this.container.find('.fh-add-another__heading').focus();
+};
