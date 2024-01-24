bf8d0612f5c506c033111b38f79f385e1291e062
Phil Davies <github@phildavies.co.uk>
2024-01-24 14:54:28 +0000

    add single text area page wip

---
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/ISingleTextAreaPageModel.cs
@@ -0,0 +1,14 @@
+﻿using FamilyHubs.SharedKernel.Razor.ErrorNext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyHubs.SharedKernel.Razor.FullPages;
+
+public interface ISingleTextAreaPageModel
+{
+    string DescriptionPartial { get; }
+
+    [BindProperty]
+    string? TextAreaValue { get; set; }
+
+    public IErrorState Errors { get; }
+}
