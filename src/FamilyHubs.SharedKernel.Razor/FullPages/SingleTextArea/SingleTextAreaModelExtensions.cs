554fc61262b510689dcf57700bfdd4463a2b4727
Phil Davies <github@phildavies.co.uk>
2024-01-25 14:55:00 +0000

    wip

--- src/FamilyHubs.SharedKernel.Razor/FullPages/SingleTextArea/SingleTextAreaModelExtensions.cs
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/SingleTextArea/SingleTextAreaModelExtensions.cs
@@ -9,7 +9,7 @@ public static class SingleTextAreaModelExtensions
         TErrorId? emptyErrorId = null)
         where TErrorId : struct, Enum
     {
-        // workaround the front end counting line endings as 1 chars (\n) as per HTML spec,
+        // workaround the front-end counting line endings as 1 chars (\n) as per HTML spec,
         // and the http transport/.net/windows using 2 chars for line ends (\r\n)
         if (model.TextAreaValue != null && model.TextAreaValue.Replace("\r", "").Length > model.TextAreaMaxLength)
         {
