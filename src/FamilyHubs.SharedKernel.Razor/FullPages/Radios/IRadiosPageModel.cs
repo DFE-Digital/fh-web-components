a947d1a218d5623eed57d842fff1a1d201b3add9
Phil Davies <github@phildavies.co.uk>
2024-01-26 13:11:33 +0000

    wip

--- src/FamilyHubs.SharedKernel.Razor/FullPages/Radios/IRadiosPageModel.cs
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/Radios/IRadiosPageModel.cs
@@ -2,17 +2,6 @@
 
 namespace FamilyHubs.SharedKernel.Razor.FullPages.Radios;
 
-public interface IRadio
-{
-    string Label { get; }
-    string Value { get; }
-    //todo:
-    ///// <summary>
-    ///// You can add hints to radio items to provide additional information about the options.
-    ///// </summary>
-    //string? Hint { get; }
-}
-
 public interface IRadiosPageModel
 {
     public IEnumerable<IRadio> Radios { get; }
