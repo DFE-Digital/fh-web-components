0b065341827ab3a7675c2ce2b678314e2e335533
Phil Davies <github@phildavies.co.uk>
2024-01-26 11:42:37 +0000

    wip

--- src/FamilyHubs.SharedKernel.Razor/FullPages/Radios/IRadiosPageModel.cs
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/Radios/IRadiosPageModel.cs
@@ -3,9 +3,31 @@ using FamilyHubs.SharedKernel.Razor.ErrorNext;
 
 namespace FamilyHubs.SharedKernel.Razor.FullPages.Radios;
 
+//public enum LabelSize
+//{
+//    h1,
+//    h2
+//}
+
+public interface IRadio
+{
+    string Label { get; }
+    string Value { get; }
+    //todo:
+    //string? Hint { get; }
+}
+
 public interface IRadiosPageModel
 {
+    public IEnumerable<IRadio> Radios { get; }
+    
+    public string? SelectedValue { get; }
+
+    //todo:
+    //public bool AreRadiosInline { get; }
+
     public IErrorState Errors { get; }
 
-    string DescriptionPartial { get; }
+    string? DescriptionPartial { get; }
+    string? Label { get; }
 }
