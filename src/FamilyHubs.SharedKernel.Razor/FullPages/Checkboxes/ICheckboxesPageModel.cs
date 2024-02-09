e4327f8878eb2af96390fde9a66520c69aa8f62b
Phil Davies <github@phildavies.co.uk>
2024-02-09 14:00:59 +0000

    add checkbox page component wip

---
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/Checkboxes/ICheckboxesPageModel.cs
@@ -0,0 +1,17 @@
+﻿using FamilyHubs.SharedKernel.Razor.ErrorNext;
+
+namespace FamilyHubs.SharedKernel.Razor.FullPages.Checkboxes;
+
+public interface ICheckboxesPageModel
+{
+    public IEnumerable<ICheckbox> Checkboxes { get; }
+
+    //todo: will need to be some sort of collection
+    public string? SelectedValue { get; set; }
+
+    public IErrorState Errors { get; }
+
+    string? DescriptionPartial { get; }
+    string? Legend { get; }
+    string? Hint { get; }
+}
