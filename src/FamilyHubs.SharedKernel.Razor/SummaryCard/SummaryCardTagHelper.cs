fd6b9f95cbac883089bba54cc2545e33eca1dc35
Phil Davies <github@phildavies.co.uk>
2024-02-23 09:29:33 +0000

    start Summary Card as Summary List

---
+++ src/FamilyHubs.SharedKernel.Razor/SummaryCard/SummaryCardTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FamilyHubs.SharedKernel.Razor.SummaryList;
+
+public class SummaryCardTagHelper : TagHelper
+{
+    public bool Border { get; set; } = true;
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "dl";
+        output.Attributes.SetAttribute("class", $"govuk-summary-list {(Border ? "" : "govuk-summary-list--no-border")}");
+        output.Content.SetHtmlContent((await output.GetChildContentAsync()).GetContent());
+    }
+}
