21d573691bb633654da0654d0ea63abb82476844
Phil Davies <github@phildavies.co.uk>
2024-02-23 11:59:49 +0000

    summary card wip

--- src/FamilyHubs.SharedKernel.Razor/Summary/SummaryCardTagHelper.cs
+++ src/FamilyHubs.SharedKernel.Razor/Summary/SummaryCardTagHelper.cs
@@ -6,6 +6,12 @@ namespace FamilyHubs.SharedKernel.Razor.Summary;
 public class SummaryCardTagHelper : TagHelper
 {
     public string? Title { get; set; }
+    public string? Action1 { get; set; }
+    public string? Action1VisuallyHidden { get; set; }
+    public string? Action1Href { get; set; }
+    public string? Action2 { get; set; }
+    public string? Action2VisuallyHidden { get; set; }
+    public string? Action2Href { get; set; }
 
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
@@ -16,14 +22,18 @@ public class SummaryCardTagHelper : TagHelper
 
         var childContent = (await output.GetChildContentAsync()).GetContent();
 
-        //var contentBuilder = new StringBuilder();
-        //contentBuilder.Append("<div class=\"govuk-summary-card__title-wrapper\">");
-        //contentBuilder.Append($"<h2 class=\"govuk-summary-card__title\">{Title}</h2>");
-        //contentBuilder.Append("</div>");
-
-        //contentBuilder.Append(childContent);
-
-        //output.Content.SetHtmlContent(contentBuilder.ToString());
+        string? actions = null;
+        if (Action1 != null || Action2 != null)
+        {
+            actions = $@"<ul class=""govuk-summary-card__actions"">
+                <li class=""govuk-summary-card__action"">
+                    <a href=""{Action1Href}"">{Action1}{(
+                Action1VisuallyHidden != null
+                ? $"<span class=\"govuk-visually-hidden\">{Action1VisuallyHidden}</span>"
+                : "")}</a>
+                </li>
+                </ul>";
+        }
 
         output.Content.SetHtmlContent($@"
             <div class=""govuk-summary-card__title-wrapper"">
