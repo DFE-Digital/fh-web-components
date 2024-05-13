ae22c6909238c91b78f701158d46144512b1d99d
Phil Davies <github@phildavies.co.uk>
2024-05-13 09:59:08 +0100

    wip

---
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/SingleAutocomplete/ISingleAutocompletePageModel.cs
@@ -0,0 +1,30 @@
+﻿using FamilyHubs.SharedKernel.Razor.ErrorNext;
+
+namespace FamilyHubs.SharedKernel.Razor.FullPages.SingleAutocomplete;
+
+public interface ISingleAutocompleteOption
+{
+    string Value { get; }
+    string Label { get; }
+}
+
+public interface ISingleAutocompletePageModel
+{
+    /// <summary>
+    /// Optional separate heading. If not supplied, the heading will be the same as the <see cref="TextBoxLabel"/>.
+    /// </summary>
+    string? HeadingText { get; }
+    string? HintText { get; }
+    string TextBoxLabel { get; }
+    string? TextBoxValue { get; }
+
+    /// <summary>
+    /// if not null, a blank disabled option will be shown in the dropdown with this value
+    /// </summary>
+    public string? DisabledOptionValue { get; }
+    IEnumerable<ISingleAutocompleteOption> Options { get; }
+
+    int? MaxLength { get; }
+
+    IErrorState Errors { get; }
+}
