af1ce5b4a8d887d780c4cbe77ee6c856a29eb95d
Phil Davies <github@phildavies.co.uk>
2024-05-15 15:54:20 +0100

    wip

--- src/FamilyHubs.SharedKernel.Razor/FullPages/SingleAutocomplete/ISingleAutocompletePageModel.cs
+++ src/FamilyHubs.SharedKernel.Razor/FullPages/SingleAutocomplete/ISingleAutocompletePageModel.cs
@@ -1,4 +1,5 @@
 ﻿using FamilyHubs.SharedKernel.Razor.ErrorNext;
+using Microsoft.AspNetCore.Html;
 
 namespace FamilyHubs.SharedKernel.Razor.FullPages.SingleAutocomplete;
 
@@ -10,6 +11,16 @@ namespace FamilyHubs.SharedKernel.Razor.FullPages.SingleAutocomplete;
 
 public interface ISingleAutocompletePageModel
 {
+    //todo: default to a built in content that uses label?
+    // label then wouldn't have to exist on ISingleAutocompletePageModel
+    // would be need a new interface for the content, IContent
+    //todo: could we handle elements that wrap the control? e.g. a div up top and /div down bottom
+    string? ContentTop => null;
+    //use something like object for complete control, a new interface or something like IDicationary<string,HtmlString>?
+    // can you have an interface for a dictionary where certain values are required
+    //todo: if our default ContentTop, create one of these with the label and it's value
+    IReadOnlyDictionary<string, HtmlString>? ContentTopSubstitutions => null;
+
     /// <summary>
     /// During GET, if not null, the option with this value will be selected by default.
     /// During POST, if the concrete's implementation of this property has the [BindProperty] attribute,
@@ -19,6 +30,7 @@ public interface ISingleAutocompletePageModel
 
     //todo: for differing content, do we force the user to have separate partials, or should we support token substitution?
     // if there are a few different subs needed, then this will get combinatory quickly
+    // we could add a content control supporting substitution and reuse that in our full page controls
     //string? DescriptionPartial => null;
 
     /// <summary>
